44d941028ac551330af9dbab116ebf4800798ceb
Christen Chawich <christen.chawich@isae.com.lb>
2020-05-06 13:04:06 +0300

    devoir tp1

--- tp1/question1/Circle.java
+++ tp1/question1/Circle.java
@@ -167,4 +167,88 @@ public class Circle {
             canvas.erase(this);
         }
     }
+    public class AuditeurCNAM {
+    /** l'attribut nom de chaque auditeur. */
+    private String nom;
+    /** l'attribut prenom de chaque auditeur. */
+    private String prenom;
+    /** l'attribut matricule de chaque auditeur. */
+    private String matricule;
+
+    /**
+     * "Création", le constructeur d'un auditeur avec son nom, son prénom et son
+     * matricule.
+     * 
+     * @param nom
+     *            le nom de l'auditeur
+     * @param prenom
+     *            son prénom
+     * @param matricule
+     *            sur la carte d'inscription, près de la photo
+     */
+    public AuditeurCNAM(String nom, String prenom, String matricule) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matricule = matricule;
+    }
+
+    /**
+     * le login au Cnam : 6 premières lettres du nom suivies de la première
+     * lettre du prénom séparées de '_' . le login retourné est en minuscules,
+     * le trait d'union, ou spéciaux <i>(pour unix)</i> sont remplacés par des
+     * '_' pas de caractères accentués pour le login voir les classes
+     * prédéfines, java.lang.String : les méthodes replaceAll, toLowerCase et
+     * substring java.lang.Math : la méthode min<br>
+     * <b>BlueJ : Menu Aide</b>
+     * 
+     * @return le login du Cnam simplifié, sans les adaptations dues aux
+     *         homonymes...
+     */
+    public String login() {
+        String login="";
+        String n=this.nom().toLowerCase().substring(0,6).replaceAll("[^a-zA-Z]+","_");
+        String p=this.prenom().toLowerCase().substring(0,1).replaceAll("[^a-zA-Z]+","_");
+        login=n+"_"+p;
+        return login;
+    }
+
+    /**
+     * Lecture du nom de l'auditeur.
+     * 
+     * @return son nom
+     */
+    public String nom() {
+        return null;// à compléter
+    }
+
+    /**
+     * Lecture du prénom de l'auditeur.
+     * 
+     * @return son prénom
+     */
+    public String prenom() {
+        return null;// à compléter
+    }
+
+    /**
+     * Lecture du matricule de l'auditeur.
+     * 
+     * @return son matricule
+     */
+    public String matricule() {
+        return null;// à compléter
+    }
+
+    /**
+     * méthode toString ( méthode redéfinie de la classe Object).
+     * 
+     * @return la concaténation du nom, du prénom et du login, selon cette
+     *         syntaxe
+     *         <code>nom() + " " + prenom() +  " login : " + login()</code>
+     */
+    @Override
+    public String toString() {
+        return nom() + " " + prenom() + " login : " + login();
+    }
+}
 }
